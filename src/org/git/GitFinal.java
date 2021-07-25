5d3f0ea2ad71869572f2b765024ebb17b337dad6
Vishnupriyajeevakumar <vinu.priya0397@gmail.com>
2021-07-25 21:57:16 +0530

    FinalCode

---
+++ src/org/git/GitFinal.java
@@ -0,0 +1,19 @@
+package org.git;
+
+public class GitFinal {
+  private void name() {
+	System.out.println("Sabaripriya");
+}
+  private void age() {
+	System.out.println("24");
+  }
+  private void gender() {
+    System.out.println("Female");
+}
+  public static void main(String[] args) {
+	  GitFinal gf = new GitFinal();
+	  gf.name();
+	  gf.age();
+	  gf.gender();
+}
+}
